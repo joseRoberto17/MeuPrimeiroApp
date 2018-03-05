3794c0f80c64be832ca7e69db61dfc8e9843822e
Jose <jose.roberto.xavier.s@gmail.com>
2018-03-05 16:24:47 -0300

    José Roberto Xavier da Silva 816113694

---
+++ app/src/main/java/com/example/wwb/meuprimeiroapp/DisplayMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.wwb.meuprimeiroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    // JOSÉ ROBERTO XAVIER DA SILVA 816113694
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
